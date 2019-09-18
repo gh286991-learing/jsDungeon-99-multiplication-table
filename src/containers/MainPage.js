a5467f568c5a011de6cebd3eae54bc5eff58454a
TomJhuang <gh286991@gmail.com>
2019-09-19 01:40:08 +0800

    mulitiplication function

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -5,10 +5,25 @@ import * as ActionCreators from '../actions';
 
 
 class MainPageContainer extends React.Component {
+  mulitiplication = () => {
+    const n = 9;
+    const m = 9;
+    const table = [];
+    for (let i = 2; i <= n; i += 1) {
+      table.push([i]);
+      for (let j = 1; j <= m; j += 1) {
+        table[i - 2].push(`${i} x ${j} = ${i * j}`);
+      }
+    }
+
+    console.log(table);
+  }
+
   render() {
     return (
       <div>
         <MainPage />
+        {this.mulitiplication()}
       </div>
     );
   }
