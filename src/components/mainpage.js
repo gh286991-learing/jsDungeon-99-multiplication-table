c3e7093c8a55f8e84f4d3eed60a25c100fb72e9c
TomJhuang <gh286991@gmail.com>
2019-09-19 01:10:57 +0800

    title

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import '../css/main.sass';
 
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
-       Hello~
+      <div className="container">
+
+        <div className="title">
+          <div className="linbox">
+            <div className="cross"> X</div>
+            <div className="line" />
+            <div className="cross"> X</div>
+
+          </div>
+          <div className="text text-top">
+         九九乘法表
+          </div>
+          <div className="text englsh">
+            MULTIPLICATION CHART
+          </div>
+
+          <div className="linbox bottom">
+            <div className="cross"> X</div>
+            <div className="line" />
+            <div className="cross"> X</div>
+          </div>
+
+        </div>
+        <div className="card" />
       </div>
     );
   }
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,8 +1,21 @@
 import { connect } from 'react-redux';
+import React from 'react';
 import MainPage from '../components/mainpage';
 import * as ActionCreators from '../actions';
+
+
+class MainPageContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <MainPage />
+      </div>
+    );
+  }
+}
+
 
 export default connect(
   (state) => ({ state }),
   ActionCreators
-)(MainPage);
+)(MainPageContainer);
